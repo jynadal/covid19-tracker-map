21d2f79c811ed6a6683ae2445352d19f8873f09c
jynadal <nadaljeanyves@hotmail.fr>
2020-06-22 23:36:05 +0200

    On Mapping Beggin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import GeoChart from "./GeoChart";
+import data from "./GeoWorld.Chart.geo.json";
+import "./App.css";
 
 function App() {
+  const [property, setProperty] = useState("pop_est");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h2>World Map with d3-geo</h2>
+      <GeoChart data={data} property={property} />
+      <h2>Select property to highlight</h2>
+      <select
+        value={property}
+        onChange={event => setProperty(event.target.value)}
+      >
+        <option value="pop_est">Population</option>
+        <option value="name_len">Name length</option>
+        <option value="gdp_md_est">GDP</option>
+      </select>
+    </React.Fragment>
   );
 }
 
