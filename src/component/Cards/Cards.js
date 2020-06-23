394fdaf461c25e1cc563cc112810dff178fbb641
jynadal <nadaljeanyves@hotmail.fr>
2020-06-23 11:21:25 +0200

    End First part of map and fetch card

---
+++ src/component/Cards/Cards.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from 'react';
+import './Cards.module.css';
+
+function Cards() {
+
+    return (
+        <h2>Return Cards and I'm not inspired!</h2>
+    )
+}
+
+export default Cards;
---
+++ src/component/Chart/Chart.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect} from 'react';
+
+import './Chart.module.css';
+
+
+function Chart() {
+//<h2>Chart in action</h2>;
+
+return (
+    <h3>Chart in action</h3>
+);
+}
+
+
+
+export default Chart;
