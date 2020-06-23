39ef2a727c2c7a13e46e58a599f27f9f671170f3
jynadal <nadaljeanyves@hotmail.fr>
2020-06-23 11:17:13 +0200

    End first part Map and beggin Card fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React, { useState } from "react";
-import GeoChart from "./GeoChart";
-import data from "./GeoWorld.Chart.geo.json";
+import { GeoChart, Cards, Chart, GeoWorld } from "./component";
+import data from "./component/GeoWorld.Chart.geo.json";
 import "./App.css";
 
 function App() {
   const [property, setProperty] = useState("pop_est");
 
   return (
-    <React.Fragment>
+    <React.Fragment>  
       <h2>World Map with d3-geo</h2>
-      <GeoChart data={data} property={property} />
+      <GeoChart  data={data} />
       <h2>Select property to highlight</h2>
-      <select
-        value={property}
-        onChange={event => setProperty(event.target.value)}
-      >
-        <option value="pop_est">Population</option>
-        <option value="name_len">Name length</option>
-        <option value="gdp_md_est">GDP</option>
-      </select>
+       <Cards  />
+       <Chart  />
     </React.Fragment>
   );
 }
